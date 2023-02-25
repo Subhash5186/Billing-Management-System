bf94dcade4557ae6d84ee886eb93e86856a227a0
Subhash5186 <103734118+Subhash5186@users.noreply.github.com>
2023-02-25 17:50:23 +0530

    Add files via upload

---
+++ Billing_Management_System/src/main/java/com/mycompany/billing_management_system/Billing_Management_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.billing_management_system;
+
+/**
+ *
+ * @author user
+ */
+public class Billing_Management_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
